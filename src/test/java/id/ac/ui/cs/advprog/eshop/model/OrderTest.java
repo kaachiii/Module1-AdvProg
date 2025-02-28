e985812a89a3b993c84f57871bc2fad0ac09f687
kaachiii <kaachi579@gmail.com>
2025-02-28 22:29:23 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,34 +8,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 class OrderTest {
+    // setUp for OrderTest
     private List<Product> products;
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         this.products = new ArrayList<>();
         Product product1 = new Product();
         product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
         product1.setProductName("Sampo Cap Bambang");
         product1.setProductQuantity(2);
-
         Product product2 = new Product();
-        product1.setProductId("a2c62328-4a37-4664-83c7-f32db8620155");
-        product1.setProductName("Sabun Cap Usep");
-        product1.setProductQuantity(1);
-
+        product2.setProductId("a2c62328-4a37-4664-83c7-f32db8620155");
+        product2.setProductName("Sabun Cap Usep");
+        product2.setProductQuantity(1);
         this.products.add(product1);
         this.products.add(product2);
     }
 
+    // unhappy path test
     @Test
     void testCreateOrderEmptyProduct(){
         this.products.clear();
 
         assertThrows(IllegalArgumentException.class, () -> {
-            Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                    this.products, 170856000L, "Safira Sudrajat");
+            Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 170856000L, "Safira Sudrajat");
         });
     }
 
+    // happy path test
     @Test
     void testCreateOrderDefaultStatus(){
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
@@ -53,13 +53,16 @@ class OrderTest {
         assertEquals("WAITING_PAYMENT", order.getStatus());
     }
 
+    // happy path test
     @Test
     void testCreateOrderSuccessStatus(){
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L, "Safira Sudrajat", "SUCCESS");
+
         assertEquals("SUCCESS", order.getStatus());
     }
 
+    // unhappy path test
     @Test
     void testCreateOrderInvalidStatus(){
         assertThrows(IllegalArgumentException.class, () -> {
@@ -68,6 +71,7 @@ class OrderTest {
         });
     }
 
+    // happy path test
     @Test
     void testSetStatusToCancelled(){
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
@@ -76,6 +80,7 @@ class OrderTest {
         assertEquals("CANCELLED", order.getStatus());
     }
 
+    // unhappy path test
     @Test
     void testSetStatusToInvalidStatus(){
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
